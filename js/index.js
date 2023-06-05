a1b80c5c779fff48f39fd68748a47f81933c99c4
William Chen <william6_chen@asus.com>
2023-06-05 18:06:16 +0800

    chore: update header mobile style

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log("test");
+document.addEventListener("DOMContentLoaded", function () {
+  const menu = document.querySelector("#menu");
+  const menuButton = document.querySelector("#menuButton");
+  const menuCircles = document.querySelectorAll("#menuButton circle");
+  const menuRect = document.querySelectorAll("#menuButton rect");
+
+  menuButton.addEventListener("click", function () {
+    menu.classList.toggle("!translate-y-0");
+
+    menuCircles.forEach(item => {
+      item.classList.toggle("hidden");
+    })
+
+    menuRect.forEach(item => {
+      item.classList.toggle("opacity-0");
+    })
+  })
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        notoSansTC: ["Inter", "NotoSansTC", ...defaultTheme.fontFamily.sans],
+        notoSansTC: ["Inter", "NotoSansTC", "Microsoft JhengHei", ...defaultTheme.fontFamily.sans],
       },
       textColor: {
         "primary": "#87BE23",
@@ -50,6 +50,29 @@ module.exports = {
         "gradient-left ": "#86BC25",
         "gradient-middle ": "#CAF57B",
         "gradient-right ": "#C9F974",
+      },
+      borderColor: {
+        "primary": "#87BE23",
+        "primary-dark": "#6C9C30",
+        "primary-lighten": "#E2EFCB",
+        "secondary-dark": "#F39313",
+        "secondary-light": "#FAB900",
+        "error-dark": "#FF0000",
+        "error-light": "#FEF0F0",
+        "gray-900": "#2A2220",
+        "gray-800": "#3F3D3C",
+        "gray-600": "#696969",
+        "gray-500": "#838383",
+        "gray-400": "#9E9EA4",
+        "gray-200": "#D9D9D9",
+        "gray-100": "#F0F0F0",
+        "gray-50 ": "#F9F9F9",
+        "gradient-left ": "#86BC25",
+        "gradient-middle ": "#CAF57B",
+        "gradient-right ": "#C9F974",
+      },
+      dropShadow: {
+        DEFAULT: "0px 0px 8px rgba(0, 0, 0, 0.2)",
       }
     },
   },

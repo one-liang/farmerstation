c899953d032e923028352df86103d1df2cc35a7b
William Chen <william6_chen@asus.com>
2023-06-06 10:07:07 +0800

    chore: update header mobile logic

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const menuCircles = document.querySelectorAll("#menuButton circle");
   const menuRect = document.querySelectorAll("#menuButton rect");
 
+  // 漢堡選單
   menuButton.addEventListener("click", function () {
     menu.classList.toggle("!top-16");
     menu.classList.toggle("!translate-y-0");
@@ -19,7 +20,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (window.innerWidth < 1024) {
     menu.addEventListener("click", function (event) {
-      event.target.classList.toggle("text-primary");
       if (event.target.classList.contains("dropdownBtn")) {
         let dropdownContent = event.target.nextElementSibling;
         dropdownContent.classList.toggle("!block");
@@ -27,6 +27,24 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+  // menu 展開收合
+  const dropdownBtns = document.querySelectorAll('.dropdownBtn');
+
+  dropdownBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      const content = btn.nextElementSibling;
+      btn.classList.toggle("text-primary");
+      const addIcon = btn.querySelector('.add');
+      const minusIcon = btn.querySelector('.minus');
+
+      content.classList.toggle('hidden');
+      addIcon.classList.toggle('hidden');
+      minusIcon.classList.toggle('hidden');
+    });
+  });
+
+
+  // menu resize 後關閉
   function adjustDropdowns() {
     const dropdowns = document.getElementsByClassName("dropdown-content");
     const windowWidth = window.innerWidth;
--- src/index.js
+++ src/index.js
@@ -4,15 +4,57 @@ document.addEventListener("DOMContentLoaded", function () {
   const menuCircles = document.querySelectorAll("#menuButton circle");
   const menuRect = document.querySelectorAll("#menuButton rect");
 
+  // 漢堡選單
   menuButton.addEventListener("click", function () {
+    menu.classList.toggle("!top-16");
     menu.classList.toggle("!translate-y-0");
 
-    menuCircles.forEach(item => {
+    menuCircles.forEach((item) => {
       item.classList.toggle("hidden");
-    })
+    });
 
-    menuRect.forEach(item => {
+    menuRect.forEach((item) => {
       item.classList.toggle("opacity-0");
-    })
-  })
-})
+    });
+  });
+
+  if (window.innerWidth < 1024) {
+    menu.addEventListener("click", function (event) {
+      if (event.target.classList.contains("dropdownBtn")) {
+        let dropdownContent = event.target.nextElementSibling;
+        dropdownContent.classList.toggle("!block");
+      }
+    });
+  }
+
+  // menu 展開收合
+  const dropdownBtns = document.querySelectorAll('.dropdownBtn');
+
+  dropdownBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      const content = btn.nextElementSibling;
+      btn.classList.toggle("text-primary");
+      const addIcon = btn.querySelector('.add');
+      const minusIcon = btn.querySelector('.minus');
+
+      content.classList.toggle('hidden');
+      addIcon.classList.toggle('hidden');
+      minusIcon.classList.toggle('hidden');
+    });
+  });
+
+
+  // menu resize 後關閉
+  function adjustDropdowns() {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth >= 1024) {
+      for (let i = 0; i < dropdowns.length; i++) {
+        dropdowns[i].classList.remove("!block");
+      }
+    }
+  }
+
+  window.addEventListener("resize", adjustDropdowns);
+});

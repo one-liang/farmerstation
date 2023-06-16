327a37cf46a2bdd44fbee162d5605a14f01d6083
william <William6_Chen@asus.com>
2023-06-16 17:11:21 +0800

    chore: update article content

--- src/article.js
+++ src/article.js
@@ -138,3 +138,69 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 // gotop end
+
+// article
+// 手風琴效果
+document.querySelectorAll(".accordion-button").forEach((button) => {
+  button.addEventListener("click", () => {
+    const accordionContent = button.nextElementSibling;
+    const accordionItem = button.parentElement;
+
+    // 閉閉所有其他面板
+    document.querySelectorAll(".accordion-content").forEach((content) => {
+      if (content !== accordionContent) {
+        content.style.maxHeight = null;
+        // accordionContent.classList.remove("py-3");
+        content.previousElementSibling
+          .querySelector(".accordion-add")
+          .classList.remove("hidden");
+        content.previousElementSibling
+          .querySelector(".accordion-minus")
+          .classList.add("hidden");
+        content.previousElementSibling
+          .querySelector("span")
+          .classList.remove("!text-primary-dark");
+      }
+    });
+
+    // 如果當前面板已經打開，我們需要將其關閉
+    if (accordionContent.style.maxHeight) {
+      accordionContent.style.maxHeight = null;
+      // accordionContent.classList.remove("py-3");
+      button.querySelector(".accordion-add").classList.remove("hidden");
+      button.querySelector(".accordion-minus").classList.add("hidden");
+      button.querySelector("span").classList.remove("!text-primary-dark");
+    } else {
+      // 否則，我們需要打開當前面板，並關閉其他所有面板
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+      // accordionContent.classList.add("py-3");
+      button.querySelector(".accordion-add").classList.add("hidden");
+      button.querySelector(".accordion-minus").classList.remove("hidden");
+      button.querySelector("span").classList.add("!text-primary-dark");
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
+});
+// article end

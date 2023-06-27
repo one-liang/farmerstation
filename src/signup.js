e3c06c9dbb4421fa292351c697dbbc3a6159e665
William Chen <william6_chen@asus.com>
2023-06-27 15:56:59 +0800

    chore: add dropdown

--- src/signup.js
+++ src/signup.js
@@ -1,2 +1,21 @@
 import "./header";
 import "./gotop";
+
+document.addEventListener('DOMContentLoaded', function () {
+  const dropdowns = document.querySelectorAll('.dropdown');
+  dropdowns.forEach(function (dropdown) {
+    const btn = dropdown.querySelector('.dropdown-btn');
+    const menu = dropdown.querySelector('.dropdown-menu');
+
+    btn.addEventListener('click', function (event) {
+      event.stopPropagation();
+      menu.classList.toggle('active');
+    });
+
+    window.addEventListener('click', function () {
+      if (menu.classList.contains('active')) {
+        menu.classList.remove('active');
+      }
+    });
+  });
+});

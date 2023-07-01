5ff8a17146858aa0bed29498184938f8e4526ad6
william <William6_Chen@asus.com>
2023-07-01 23:40:59 +0800

    update

--- src/farmers.js
+++ src/farmers.js
@@ -1,2 +1,51 @@
 import "./header";
 import "./gotop";
+
+document.addEventListener("DOMContentLoaded", function () {
+  // 定義地區列表
+  let regions = [
+    "Taipei",
+    "Taoyuan",
+    "Hsinchu",
+    "Miaoli",
+    "Taichung",
+    "Changhua",
+    "Yunlin",
+    "Chiayi",
+    "Tainan",
+    "Kaohsiung",
+    "Pingtung",
+    "Yilan",
+    "Hualien",
+    "Taitung",
+    "Nantou",
+  ];
+
+  // 對每個地區進行操作
+  regions.forEach((region) => {
+    // 檢查是否有地區的卡片
+    let cardElement = document.querySelector(`.${region}-card`);
+
+    // 如果有地區的卡片
+    if (cardElement) {
+      // 將 active class 加入對應的 wrapper
+      let wrapperElement = document.querySelector(`.${region}-wrapper`);
+      if (wrapperElement) {
+        wrapperElement.classList.add("active");
+      }
+    }
+  });
+
+  let wrappers = document.querySelectorAll(".wrapper");
+  wrappers.forEach((wrapper) => {
+    wrapper.addEventListener("click", function () {
+      // 移除所有 wrapper 的 active 類別
+      wrappers.forEach((wrapper) => {
+        wrapper.classList.remove("active");
+      });
+
+      // 將 active 類別添加到被點擊的 wrapper
+      this.classList.add("active");
+    });
+  });
+});
